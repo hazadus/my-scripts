ce0192965c888e0943ef7fce03e57eee9c301780
hazadus <hazadus7@gmail.com>
2025-08-26 08:29:18 +0300

    feat(clipmon.py): добавить поддержку сохранения изображений из буфера обмена
    - Реализована обработка PNG-изображений из буфера обмена macOS
    - Добавлена функция `save_png_clip` для сохранения изображений в формате PNG
    - Создаются отдельные папки для текста (`~/clipmon/text`) и изображений (`~/clipmon/images`)
    - Расширен раздел `README.md` с описанием новых возможностей и примеров использования

--- scripts/clipmon.py
+++ scripts/clipmon.py
@@ -13,10 +13,11 @@ import time
 from datetime import datetime
 from pathlib import Path
 
-from AppKit import NSPasteboard, NSStringPboardType
+from AppKit import NSPasteboard, NSPasteboardTypePNG, NSStringPboardType
 from loguru import logger
 
 TEXT_CLIPS_PATH = Path.home() / "clipmon" / "text"
+PNG_CLIPS_PATH = Path.home() / "clipmon" / "images"
 
 
 def main():
@@ -28,20 +29,43 @@ def monitor_clipboard(interval: int = 5):
     last_content = None
     try:
         while True:
-            current_content = get_clipboard_content()
+            current_content, extension = get_clipboard_content()
             if current_content != last_content:
-                if current_content:
-                    save_text_clip(current_content)
+                process_content(current_content, extension)
                 last_content = current_content
             time.sleep(interval)
     except KeyboardInterrupt:
         logger.info("🛑 Мониторинг буфера обмена остановлен пользователем.")
 
 
+def process_content(content, extension):
+    if content is None:
+        return
+
+    match extension:
+        case "txt":
+            save_text_clip(content)
+        case "png":
+            save_png_clip(content)
+
+
 def get_clipboard_content():
     pb = NSPasteboard.generalPasteboard()
-    content = pb.stringForType_(NSStringPboardType)
-    return content if content is not None else ""
+    content = None
+    file_extension = None
+
+    if NSPasteboardTypePNG in pb.types():
+        data = pb.dataForType_(NSPasteboardTypePNG)
+        if data:
+            content = data
+            file_extension = "png"
+    elif NSStringPboardType in pb.types():
+        text = pb.stringForType_(NSStringPboardType)
+        if text:
+            content = text
+            file_extension = "txt"
+
+    return content, file_extension
 
 
 def save_text_clip(text: str):
@@ -53,7 +77,19 @@ def save_text_clip(text: str):
     with open(clip_path, "w", encoding="utf-8") as f:
         f.write(text)
 
-    logger.info(f"✅ Текст из буфера сохранён в {clip_path}")
+    logger.info(f"✅📝 Текст сохранён в {clip_path}")
+
+
+def save_png_clip(data):
+    PNG_CLIPS_PATH.mkdir(parents=True, exist_ok=True)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    clip_path = PNG_CLIPS_PATH / f"clip_{timestamp}.png"
+
+    with open(clip_path, "wb") as f:
+        f.write(data)
+
+    logger.info(f"✅🖼️ Изображение сохранено в {clip_path}")
 
 
 if __name__ == "__main__":
